0c2badda7dcf0e4c3332977870d58ec188ef78ab
INuLLiTyI <vovanstalmakov.ru19@gmail.com>
2025-09-17 01:37:15 +0300

    Adding features to new pipe

--- pipe.cpp
+++ pipe.cpp
@@ -22,3 +22,8 @@ string Pipe::GetName() { return Name; }
 bool Pipe::GetStatement() { return InRepair; }
 int Pipe::GetDiameter() { return Diameter; }
 bool Pipe::IsConnected() { return CS1 != 0 && CS2 != 0; }
+
+void Pipe::SetName(const string& name) { Name = name; }
+void Pipe::SetLength(int length) { Length = length; }
+void Pipe::SetDiameter(int diameter) { Diameter = diameter; }
+void Pipe::SetInRepair(bool inRepair) { InRepair = inRepair; }
