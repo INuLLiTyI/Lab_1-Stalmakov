68d53eb3d3397b2613acc7dd291b7a9fbcf2c527
INuLLiTyI <vovanstalmakov.ru19@gmail.com>
2025-09-17 02:02:52 +0300

    Starting making of UI of the application

--- lab_code_execution.cpp
+++ lab_code_execution.cpp
@@ -1,10 +1,21 @@
-#include <cmath>
-#include <chrono>
-#include <format>
+#include <iostream>
+#include <fstream>
+#include <limits>
+#include "pipe.h"
+#include "CompressorStation.h"
 
 using namespace std
 
-int main() {
-
+void printMenu() {
+    cout << "\n=== Pipeline Management System ===\n";
+    cout << "1. Add Pipe\n";
+    cout << "2. Add Compressor Station\n";
+    cout << "3. View all objects\n";
+    cout << "4. Edit Pipe\n";
+    cout << "5. Edit Compressor Station\n";
+    cout << "6. Save\n";
+    cout << "7. Load\n";
+    cout << "0. Exit\n";
+    cout << "Choose option: ";
 }
 
