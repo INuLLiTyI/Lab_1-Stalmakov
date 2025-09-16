357fbcdec5c7607e1324c0a4ddffeca09c2c2e10
INuLLiTyI <vovanstalmakov.ru19@gmail.com>
2025-09-17 00:42:00 +0300

    Creation of class pipe in the header file with specified atributes. Common realisation of use contruct metod in pipe.cpp

---
+++ pipe.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+using namespace std
+
+int Pipe::maxId = 0;
+
+Pipe::Pipe()
+{
+    id = 0;
+    Name = "None";
+    Length = 0;
+    Diameter = 0;
+    InRepair = 0;
+
+    CS1 = 0;
+    CS2 = 0;
+}
---
+++ pipe.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <istream>
+#include <unordered_map>
+
+class Pipe {
+private:
+	static int maxId;
+	int id;
+	std::string Name;
+	int Length;
+	int Diameter;
+	bool InRepair;
+
+public:
+	int CS1;
+	int CS2;
+
+	Pipe();
+
+	int GetId();
+	static int GetMaxId();
+	std::string GetName();
+	bool GetStatement();
+	int GetDiameter();
+	bool IsConnected();
+};
