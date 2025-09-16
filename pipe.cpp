bc2268a9e0de5f96f4cc789b8a7d0187f568fe56
INuLLiTyI <vovanstalmakov.ru19@gmail.com>
2025-09-17 01:34:34 +0300

    Calling getter funcs to know attributes of new Pipe

--- pipe.cpp
+++ pipe.cpp
@@ -15,3 +15,10 @@ Pipe::Pipe()
     CS1 = 0;
     CS2 = 0;
 }
+
+int Pipe::GetId() { return id; }
+int Pipe::GetMaxId() { return maxId; }
+string Pipe::GetName() { return Name; }
+bool Pipe::GetStatement() { return InRepair; }
+int Pipe::GetDiameter() { return Diameter; }
+bool Pipe::IsConnected() { return CS1 != 0 && CS2 != 0; }
